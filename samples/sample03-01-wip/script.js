f9c1c35e0b710a90247947407e9141b0b0d17d78
sanko-shoko <93602006+wizroot-dev@users.noreply.github.com>
2023-01-07 11:32:13 +0900

    update

--- samples/sample03-01-wip/script.js
+++ samples/sample03-01-wip/script.js
@@ -1,8 +1,8 @@
 function main() {
     xnew(() => {
         const screen = xnew(xn.Screen, { width: 400, height: 300 });
-
-        const renderer = PIXI.autoDetectRenderer({ view: screen.canvas, width: screen.width, height: screen.height, background: '#000000' });
+        
+        const renderer = PIXI.autoDetectRenderer({ view: screen.canvas, width: screen.width, height: screen.height });
         const stage = new PIXI.Container();
 
         xnew(Input);
@@ -17,43 +17,37 @@ function main() {
 }
 
 function Input({ node }) {
-    if (navigator.userAgent.match(/iPhone|iPad|Android.+Mobile/)) {
+    if (1 || xutil.device.isMobile()) {
         const stick = xnew({ style: 'position: absolute; left: 0px; bottom: 0px; z-index: 10;' }, xn.AnalogStick, { size: 160 });
-        stick.on('start move', (event, ex) => {
-            stick.emit('#move', { vector: ex.vector });
+        stick.on('start move end', (event, ex) => {
+            event.stopPropagation();
+            node.emit('#move', { x: ex.vector.x * 0.7, y: ex.vector.y * 0.7 });
+        });
+        stick.on('click', (event) => {
+            event.stopPropagation();
         });
 
         const button = xnew({ style: 'position: absolute; right: 20px; bottom: 20px; z-index: 10;' }, xn.CircleButton);
-        button.on('down', () => {
-            node.emit('#shot', true);
-        });
-        button.on('up', () => {
-            node.emit('#shot', false);
+        button.on('down up', (event, ex) => {
+            event.stopPropagation();
+            node.emit('#shot', ex.type === 'down');
         });
     } else {
         const keyState = {};
-        node.on('keydown', (event) => {
-            keyState[event.key] = 1;
-            move();
-            if (event.key === ' ') node.emit('#shot', true);
-        });
-        node.on('keyup', (event) => {
-            keyState[event.key] = 0;
-            move();
-            if (event.key === ' ') node.emit('#shot', false);
-        });
+        node.on('keydown keyup', (event) => {
+            keyState[event.key] = event.type === 'keydown';
 
-        function move() {
             const x = (keyState['ArrowLeft'] ? -1 : 0) + (keyState['ArrowRight'] ? +1 : 0);
             const y = (keyState['ArrowUp'] ? -1 : 0) + (keyState['ArrowDown'] ? +1 : 0);
-            node.emit('#move', { vector: { x, y } });
-        }
+
+            node.emit('#move', { x, y });
+            node.emit('#shot', keyState[' ']);
+        });
     }
 }
 
 function Title({ node, screen, stage }) {
-    
-    const text = new PIXI.Text('touch start', new PIXI.TextStyle({ fill: '#FFFFFF' }));
+    const text = new PIXI.Text('touch start', { fill: '#FFFFFF' });
     text.x = screen.width / 2;
     text.y = screen.height / 2;
     text.pivot.x = text.width / 2;
@@ -64,7 +58,7 @@ function Title({ node, screen, stage }) {
     node.on('click', () => {
         xnew(node.parent, GameMain, { screen, stage })
         node.finalize();
-    })
+    });
     return {
         finalize: () => {
             stage.removeChild(text);
@@ -74,16 +68,22 @@ function Title({ node, screen, stage }) {
 
 function GameMain({ node, screen, stage }) {
     const scene = stage.addChild(new PIXI.Container());
-    
-    node.setTimer(100, () => {
+    xnew(Score, { screen, scene });
+
+    const id = node.setTimer(500, () => {
         xnew(Enemy, { screen, scene });
     }, true);
 
     xnew(Player, { screen, scene });
 
     node.on('#gameover', () => {
-        xnew(node.parent, GameOver, { screen, stage })
-        node.finalize();
+        node.clearTimer(id);
+        xnew(GameOver, { screen, scene });
+
+        node.on('click', () => {
+            xnew(node.parent, Title, { screen, stage })
+            node.finalize();
+        })
     });
 
     return {
@@ -95,40 +95,36 @@ function GameMain({ node, screen, stage }) {
     };
 }
 
-function GameOver({ node, screen, stage }) {
-  
-    const text = new PIXI.Text('game over');
+function GameOver({ node, screen, scene }) {
+    const text = new PIXI.Text('game over', { fill: '#FFFFFF' });
     text.x = screen.width / 2;
     text.y = screen.height / 2;
     text.pivot.x = text.width / 2;
     text.pivot.y = text.height / 2;
 
-    stage.addChild(text);
+    scene.addChild(text);
 
-    node.on('click', () => {
-        xnew(node.parent, Title, { screen, stage })
-        node.finalize();
-    });
     return {
         finalize: () => {
-            stage.removeChild(text);
+            scene.removeChild(text);
         }
     }
 }
 
 function Player({ node, screen, scene }) {
+    const object = new PIXI.Container();
+    scene.addChild(object);
+    const sprite = new PIXI.Sprite();
+    object.addChild(sprite);
 
-    const object = scene.addChild(new PIXI.Container());
-    const graphics = object.addChild(new PIXI.Graphics());
-    graphics.beginFill(0xEA1E63);
-    graphics.drawRect(-10, -10, 20, 20);
-    graphics.endFill();
+    sprite.texture = new PIXI.Texture(PIXI.Texture.from('texture.png'), new PIXI.Rectangle(0, 0, 32, 32));
+    sprite.anchor.set(0.5);
 
     object.x = screen.width / 2;
     object.y = screen.height / 2;
 
     let velocity = { x: 0, y: 0 };
-    node.on('#move', ({ vector }) => { velocity = vector; });
+    node.on('#move', (vector) => { velocity = vector; });
 
     let shot = false;
     let stanby = true;
@@ -147,7 +143,7 @@ function Player({ node, screen, scene }) {
                 const distance = Math.sqrt(dx * dx + dy * dy);
                 if (distance < 22) {
                     enemy.finalize();
-                    break;
+                    node.die();
                 }
             }
             
@@ -156,6 +152,10 @@ function Player({ node, screen, scene }) {
                 stanby = false;
                 node.setTimer(200, () => { stanby = true; });
             }
+        },
+        die: () => {
+            node.emit('#gameover');
+            node.finalize();
         },
         finalize: () => {
             scene.removeChild(object);
@@ -172,7 +172,7 @@ function Bullet({ node, screen, scene, position }) {
     const graphics = object.addChild(new PIXI.Graphics());
     graphics.lineStyle(0);
     graphics.beginFill(0xDE3249, 1);
-    graphics.drawCircle(0, 0, 4);
+    graphics.drawCircle(0, 0, 3);
     graphics.endFill();
 
     return {
@@ -202,13 +202,9 @@ function Bullet({ node, screen, scene, position }) {
 function Enemy({ node, screen, scene }) {
     node.key = 'enemy';
 
-    const texture = PIXI.Texture.from('texture.png');
-    const texture1 = new PIXI.Texture(texture, new PIXI.Rectangle(0, 0, 32, 32));
-
     const object = scene.addChild(new PIXI.Container());
-
     const sprite = new PIXI.Sprite();
-    sprite.texture = texture1;
+    sprite.texture = new PIXI.Texture(PIXI.Texture.from('texture.png'), new PIXI.Rectangle(32, 0, 32, 32));
     sprite.anchor.set(0.5);
     object.addChild(sprite);
 
@@ -216,18 +212,27 @@ function Enemy({ node, screen, scene }) {
     object.x = x
     object.y = -10;
 
+    const a = Math.PI * (Math.random() * 90 + 45) / 180;
+    const v = (Math.random() * 2 + 1);
+    const velocity = { x: v * Math.cos(a), y: v * Math.sin(a)};
+
     return {
-        update: (time) => {
-            object.y += 2;
-            object.x = x + 50 * Math.sin(time / 1000);
-            if (object.y > screen.height) {
-                node.finalize();
-            }
+        update: () => {
+            if (object.x < 10) velocity.x = Math.abs(velocity.x);
+            if (object.x >= screen.width - 10) velocity.x = -Math.abs(velocity.x);
+            if (object.y < 10) velocity.y = Math.abs(velocity.y);
+            if (object.y >= screen.height - 10) velocity.y = -Math.abs(velocity.y);
+
+            object.x += velocity.x;
+            object.y += velocity.y;
         },
-        die: () => {
+        die: (value = 1) => {
+            const position = { x: object.x, y: object.y };
             for(let i = 0; i < 3; i++) {
-                xnew(node.parent, Star, { screen, scene, position: { x: object.x, y: object.y } });
+                xnew(node.parent, Star, { screen, scene, position, value });
             }
+            xnew(node.parent, ScoreUp, { screen, scene, position, value });
+
             node.finalize();
         },
         finalize: () => {
@@ -237,43 +242,79 @@ function Enemy({ node, screen, scene }) {
     };
 }
 
-function Star({ node, screen, scene, position }) {
+function Star({ node, screen, scene, position, value = 1 }) {
     const object = scene.addChild(new PIXI.Container());
     object.x = position.x;
     object.y = position.y;
 
-    const graphics = object.addChild(new PIXI.Graphics());
-    graphics.lineStyle(0);
-    graphics.beginFill(0x35CC5A, 1);
-    graphics.drawCircle(0, 0, 5);
-    graphics.endFill();
+    const sprite = new PIXI.Sprite();
+    sprite.texture = new PIXI.Texture(PIXI.Texture.from('texture.png'), new PIXI.Rectangle(64, 0, 32, 32));
+    sprite.anchor.set(0.5);
+    object.addChild(sprite);
 
     const vector = { x: (2 * Math.random() - 1) * 3, y: (2 * Math.random() - 1) * 3 };
-    console.log(position, vector);
-    node.setTimer(800, () => {
-        node.finalize();
-    })
+    node.setTimer(800, () => node.finalize());
 
+    let counter = 0;
     return {
         update: () => {
             object.x += vector.x;
             object.y += vector.y;
+            object.rotation = counter++ / 10;
 
             for (const enemy of xfind('enemy')) {
                 const dx = node.object.x - enemy.object.x;
                 const dy = node.object.y - enemy.object.y;
                 const distance = Math.sqrt(dx * dx + dy * dy);
                 if (distance < 15) {
-                    enemy.die();
+                    enemy.die(value * 2);
                     node.finalize();
                     break;
                 }
             }
         },
         finalize: () => {
-            console.log('lost')
             scene.removeChild(object);
         },
         object: { get: () => object },
     };
 }
+
+function ScoreUp({ node, screen, scene, position, value = 1 }) {
+    const text = new PIXI.Text(`+ ${value}`, { fontSize: 16, fill: '#FFFF22' });
+    text.x = position.x;
+    text.y = position.x;
+    text.pivot.x = text.width / 2;
+    text.pivot.y = text.height / 2;
+
+    node.emit('#scoreup', value);
+    scene.addChild(text);
+    node.setTimer(1000, () => node.finalize());
+
+    let counter = 0;
+    return {
+        update: () => {
+            text.y = position.y - 30 * Math.exp(-counter / 20) * Math.abs(Math.sin(Math.PI * (counter * 10) / 180)); 
+            counter++;
+        },
+        finalize: () => {
+            scene.removeChild(text);
+        }
+    }
+}
+
+function Score({ node, screen, scene }) {
+    let score = 0;
+
+    const text = new PIXI.Text(`score ${score}`, { fontSize: 16, fill: '#FFFF22' });
+    text.x = screen.width;
+    text.y = 0;
+    text.pivot.x = text.width;
+    text.pivot.y = 0;
+    scene.addChild(text);
+    node.on('#scoreup', (value) => {
+        text.text = `score ${score += value}`;
+        text.pivot.x = text.width;
+    });
+}
+
--- samples/xnew.js
+++ samples/xnew.js
@@ -483,276 +483,16 @@
       };
   })();
 
-
-  //----------------------------------------------------------------------------------------------------
-  // audio 
-  //----------------------------------------------------------------------------------------------------
-
-  const audio = (() => {
-      const context = new (window.AudioContext || window.webkitAudioContext);
-      const store = new Map();
-
-      return {
-          create: (props) => new Effect(props),
-          load: (path) => new Music(path),
-      };
-
-      function createAudioNode(name, input = null, output = null) {
-          const node = context[`create${name}`]();
-          if (input) input.connect(node);
-          if (output) node.connect(output);
-          return node;
-      }
-
-      function StandardNode({ volume = null, pan = null, echo = null, reverb = null } = {}) {
-          const nodes = {};
-
-          if (echo) {
-              echo = Object.assign({ delay: 300, feedback: 0.3 }, (typeof echo === 'object' && echo !== null) ? echo : {});
-          }
-          if (reverb) {
-              reverb = Object.assign({ duration: 2000, decay: 2 }, (typeof reverb === 'object' && reverb !== null) ? reverb : {});
-          }
-
-          // volumeNode
-          {
-              nodes.volumeNode = createAudioNode('Gain');
-              nodes.volumeNode.gain.value = volume;
-          }
-          
-          // volumeNode -> panNode -> destination
-          if (context.createStereoPanner){
-              nodes.panNode = createAudioNode('StereoPanner', nodes.volumeNode);
-              nodes.panNode.pan.value = pan;
-          } else {
-              nodes.panNode = createAudioNode('Panner', nodes.volumeNode);
-              nodes.panNode.setPosition(pan, 0, 1 - Math.abs(pan));
-          }
-
-          // volumeNode -> convolverNode -> panNode
-          if (reverb) {
-              nodes.convolverNode = createAudioNode('Convolver', nodes.volumeNode, nodes.panNode);
-              nodes.convolverNode.buffer = impulseResponse(reverb);
-          }
-
-          // volumeNode -> delayNode(feelbackNode ->filterNode ->delayNode) -> panNode
-          if (echo) {
-              nodes.delayNode = createAudioNode('Delay', nodes.volumeNode, nodes.panNode);
-              nodes.delayNode = context.createDelay();
-              nodes.delayNode.delayTime.value = echo.delay / 1000;
-              
-              nodes.feedbackNode = createAudioNode('Gain', nodes.delayNode, nodes.delayNode);
-              nodes.feedbackNode.gain.value = echo.feedback;
-              nodes.delayNode.connect(nodes.feedbackNode);
-              // if (echo.filter > 0) {
-              //     nodes.filterNode = context.createBiquadFilter();
-              //     nodes.filterNode.frequency.value = echo.filter;
-              //     nodes.feedbackNode.connect(nodes.filterNode);
-              //     nodes.filterNode.connect(nodes.delayNode);
-              // } else {
-              //     nodes.feedbackNode.connect(nodes.delayNode);
-              // }
-          }
-
-          this.fade = (duration, volume, wait = 0) => {
-              nodes.volumeNode.gain.linearRampToValueAtTime(nodes.volumeNode.gain.value, context.currentTime + wait / 1000);
-              nodes.volumeNode.gain.linearRampToValueAtTime(volume, context.currentTime + (wait + duration) / 1000);
-          };
-
-          Object.defineProperties(this, {
-              input: {
-                  get: () => nodes.volumeNode,
-              },
-              output: {
-                  get: () => nodes.panNode,
-              },
-              volume: {
-                  set: (value) => nodes.volumeNode.gain.value = value,
-                  get: () => nodes.volumeNode.gain.value,
-              },
-              pan: {
-                  set: (value) => nodes.panNode.pan.value = value,
-                  get: () => nodes.panNode.pan.value,
-              },
-          });
-      }
-
-      function Music(path) {
-          let data = null;
-          if (store.has(path)) {
-              data = store.get(path);
-          } else {
-              data = {};
-              data.buffer = null;
-              data.promise = fetch(path)
-                  .then((response) => response.arrayBuffer())
-                  .then((response) => context.decodeAudioData(response))
-                  .then((response) => data.buffer = response)
-                  .catch(() => {
-                      console.warn(`"${path}" could not be loaded.`);
-                  });
-              store.set(path, data);
-          }
-
-          let sourceNode = null;
-          let standardNode = null;
-          let startTime = 0;
-          let state = { volume: 1.0, pan: 0.0 };
-
-          Object.defineProperties(this, {
-              isReady: { value: () => data.buffer ? true : false, },
-              promise: { get: () => data.promise, },
-              volume: {
-                  set: (value) => { 
-                      state.volume = value;
-                      if (standardNode) standardNode.volume = value;
-                  },
-                  get: () => {
-                      return standardNode ? standardNode.volume : state.volume;
-                  },
-              },
-              pan: {
-                  set: (value) => {
-                      state.pan = value;
-                      if (standardNode) standardNode.pan = value;
-                  },
-                  get: () => {
-                      return standardNode ? standardNode.pan : state.pan;
-                  },
-              },
-          });
-      
-          this.play = ({ offset = 0, volume = null, pan = null, loop = false, fadeIn = null, echo = null, reverb = null } = {}) => {           
-              if (this.isReady() === false) return;
-              this.pause();
-
-              startTime = context.currentTime;
-              state.volume = volume ?? state.volume;
-              state.pan = pan ?? state.pan;
-
-              sourceNode = createAudioNode('BufferSource');
-              sourceNode.buffer = data.buffer;
-              sourceNode.playbackRate.value = 1;
-              
-              standardNode = new StandardNode({ volume: state.volume, pan: state.pan, echo, reverb });
-              sourceNode.connect(standardNode.input);
-              standardNode.output.connect(context.destination);
-
-              if (fadeIn) {
-                  standardNode.volume = 0;
-                  this.fade(fadeIn. state.volume);
-              }
-              sourceNode.loop = loop;
-              sourceNode.start(0, offset);
-          };
-      
-          this.pause = ({ fadeOut = 0.0, } = {}) => {
-              if (sourceNode) {
-                  if (fadeOut) {
-                      standardNode.fade(fadeOut, 0);
-                  }
-                  setTimeout(() => sourceNode.stop(0), fadeOut);
-
-                  state.volume = standardNode.volume;
-                  return (context.currentTime - startTime) % data.buffer.duration;
-              }
-          };
-
-      }
-      function Effect({
-          waveform = 'sine',  //waveform type: "sine", "triangle", "square", "sawtooth"
-          frequency = 200,    //The sound's fequency pitch in Hertz
-          attack = 0,              //The time, in seconds, to fade the sound in
-          decay = 1,               //The time, in seconds, to fade the sound out
-          volume = 1,         //The sound's maximum volume
-          pan = 0,            //The speaker pan. left: -1, middle: 0, right: 1
-          wait = 0,                //The time, in seconds, to wait before playing the sound
-          pitchBend = null,     //The number of Hz in which to bend the sound's pitch down
-          dissonance = 0,          //A value in Hz. It creates 2 dissonant frequencies above and below the target pitch
-          echo = null,                //An array: [delayTimeInSeconds, feedbackTimeInSeconds, filterValueInHz]
-          reverb = null,              //An array: [durationInSeconds, decayRateInSeconds, reverse]
-          timeout = 2,             //A number, in seconds, which is the maximum duration for sound effects
-      }){
-          let standardNode = null;
-          this.play = () => {
-
-              const oscillator = context.createOscillator();
-              oscillator.type = waveform;
-              oscillator.frequency.value = frequency;
-          
-              standardNode = new StandardNode({ volume, pan, echo, reverb });
-              oscillator.connect(standardNode.input);
-              standardNode.output.connect(context.destination);
-
-              // if (attack) {
-              //     standardNode.volume = 0;
-              //     standardNode.fade(attack. volume, wait);
-              // }
-              standardNode.fade(decay, 0, wait + attack);
-
-              if (pitchBend){
-                  oscillatorNode.frequency.linearRampToValueAtTime(frequency, context.currentTime + wait / 1000);
-                  oscillatorNode.frequency.linearRampToValueAtTime(frequency + pitchBend, context.currentTime +(wait + attack + decay) / 1000);
-              }
-              if (dissonance > 0){
-                  const d1 = context.createOscillator();
-                  const d2 = context.createOscillator();
-          
-                  //Connect the oscillators to the gain and destination nodes
-                  d1.connect(standardNode.input);
-                  d2.connect(standardNode.input);
-          
-                  //Set the waveform to "sawtooth" for a harsh effect
-                  d1.type = "sawtooth";
-                  d2.type = "sawtooth";
-          
-                  //Make the two oscillators play at frequencies above and
-                  //below the main sound's frequency. Use whatever value was
-                  //supplied by the `dissonance` argument
-                  d1.frequency.value = frequency + dissonance;
-                  d2.frequency.value = frequency - dissonance;
-          
-                  play(d1);
-                  play(d2);
-              }
-          
-              //Play the sound
-              play(oscillator);
-          
-              //The `play` function
-              function play(node) {
-                  node.start(context.currentTime + wait / 1000);
-          
-                  node.stop(context.currentTime + (wait + timeout) / 1000);
-              }        
-          }; 
-      }
-      
-      function impulseResponse({ duration, decay }) {
-          const length = context.sampleRate * duration / 1000;
-          const impulse = context.createBuffer(2, length, context.sampleRate);
-      
-          const ch0 = impulse.getChannelData(0);
-          const ch1 = impulse.getChannelData(1);
-          for (let i = 0; i < length; i++) {
-              ch0[i] = (2 * Math.random() - 1) * Math.pow(1 - i / length, decay);
-              ch1[i] = (2 * Math.random() - 1) * Math.pow(1 - i / length, decay);
-          }
-          return impulse;
-      }
-  })();
-
   var util = {
     __proto__: null,
-    device: device,
-    audio: audio
+    device: device
   };
 
   //----------------------------------------------------------------------------------------------------
   // screen
   //----------------------------------------------------------------------------------------------------
 
-  function Screen({ node, width, height, objectFit = 'contain', pixelated = true }) {
+  function Screen({ node, width = 640, height = 480, objectFit = 'contain', pixelated = true }) {
       node.nestElement({ style: 'position: relative; width: 100%; height: 100%; overflow: hidden;' });
       node.nestElement({ style: 'position: absolute; inset: 0; margin: auto;' });
       node.nestElement({ style: 'position: relative; width: 100%; height: 100%;' });
@@ -890,9 +630,6 @@
       const draw = xnew(DrawEvent);
 
       draw.on('start move', (event, ex) => {
-          event.preventDefault();
-          event.stopPropagation();
-
           target.element.style.filter = 'brightness(90%)';
 
           const [x, y] = [ex.end.x - size / 2, ex.end.y - size / 2];
@@ -935,14 +672,14 @@
           if (state === 0) {
               state = 1;
               target.element.style.filter = 'brightness(90%)';
-              node.emit('down', event);
+              node.emit('down', event, { type: 'down' });
           }
       });
       win.on('touchend mouseup', (event) => {
           if (state === 1) {
               state = 0;
               target.element.style.filter = '';
-              node.emit('up', event);
+              node.emit('up', event, { type: 'up' });
           }
       });
   }
